968091d2292d6bc1caec0914644caa75901fdd9c
zhangyushao <zhangyushaow@gmail.com>
2024-03-23 12:31:25 +0000

    Update blog post date display

--- src/routes/blogs/[link]/+page.svelte
+++ src/routes/blogs/[link]/+page.svelte
@@ -5,10 +5,10 @@
 
 <div>
 	{#if data.post}
-		<h1>{data.post.date}</h1>
 		<article
 			class="prose mx-auto md:prose-lg prose-pre:m-0 prose-pre:rounded-none prose-pre:overflow-x-auto"
 		>
+			<h4>{data.post.date}</h4>
 			{@html data.post.content}
 		</article>
 	{:else if data.error}
